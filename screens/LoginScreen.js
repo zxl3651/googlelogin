5259e9a4de754b02f2815a0cd86df97ccd03927c
S_HYEON <zxl3651@naver.com>
2023-05-17 00:57:59 +0900

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import * as Google from "expo-google-app-auth";
+
+const LoginScreen = ({ navigation }) => {
+  const signInAsync = async () => {
+    console.log("LoginScreen.js 6 | loggin in");
+    try {
+      const { type, user } = await Google.logInAsync({
+        iosClientId: "733362910114-18ie5k65aedf528rk6tk7s8f2rb93nh6.apps.googleusercontent.com",
+        androidClientId: `<YOUR_ANDROID_CLIENT_ID>`,
+      });
+
+      if (type === "success") {
+        // Then you can use the Google REST API
+        console.log("LoginScreen.js 17 | success, navigating to profile");
+        navigation.navigate("Profile", { user });
+      }
+    } catch (error) {
+      console.log("LoginScreen.js 19 | error with login", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Login with Google" onPress={signInAsync} />
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
